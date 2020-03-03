a311eec98756b3cdbca4dbfa3ca8cc88146bca1e
lanshuye <927980270@qq.com>
2020-03-03 20:50:12 +0800

    <AutoUpload>

--- WebUI.js
+++ WebUI.js
@@ -9,16 +9,51 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var net = __importStar(require("net"));
 var Core = __importStar(require("./Core"));
+var fs = __importStar(require("fs"));
 var MainConnect;
 var MainGroup;
 var ServerWeb = new net.Server();
 ServerWeb.listen(8081, "0.0.0.0", function () {
     ServerWeb.on("connection", function (UserSock) {
         UserSock.on("data", function (data) {
-            if (MainConnect != undefined) {
-                Core.frame.SendMsg(MainConnect, MainGroup, data.toString());
+            if (data.toString().indexOf(" ") != 0) {
+                var path = data.toString().split(" ");
+                if (path[1] == "/") {
+                    fs.readFile("./WebUI.html", function (err, data) {
+                        UserSock.write("HTTP/1.1 200 OK\r\nContent-Length: " + data.length + "\r\n\r\n");
+                        UserSock.write(data);
+                        UserSock.end();
+                    });
+                }
+                else if (path[1].substr(0, 5) == "/OAC_") {
+                    var Do = path[1].replace("/OAC_", "");
+                    if (Do == "GETMESSAGE") {
+                        fs.readFile("./Message.json", function (err, data) {
+                            UserSock.write("HTTP/1.1 200 OK\r\nContent-Length: " + data.length + "\r\n\r\n");
+                            UserSock.write(data);
+                            UserSock.end();
+                        });
+                    }
+                    else if (Do.substr(0, new String("SETMESSAGE_").length) == "SETMESSAGE_") {
+                        var Token = Do.replace("SETMESSAGE_", "");
+                        var T2 = Token.split("_");
+                        fs.readFile("./Message.json", function (err, data) {
+                            var data_obj = JSON.parse(data.toString());
+                            data_obj[T2[1]] = T2[0];
+                            fs.writeFile("./Message.json", JSON.stringify(data_obj), function (err) {
+                                UserSock.write("HTTP/1.1 200 OK\r\nContent-Length: " + new String("DONE").length + "\r\n\r\n");
+                                UserSock.write(new String("DONE").valueOf());
+                                UserSock.end();
+                            });
+                        });
+                    }
+                }
+                else {
+                }
             }
-            UserSock.end();
+            else {
+                UserSock.end("HTTP/1.1 404");
+            }
         });
         UserSock.on("close", function () {
             UserSock.end();
