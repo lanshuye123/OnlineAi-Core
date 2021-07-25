a2d88d1806eb08ca50a7d93689506912f9133993
lanshuye <927980270@qq.com>
2021-07-25 10:39:54 +0800

    Common Update

--- Run.bat
+++ Run.bat
@@ -1,10 +1,12 @@
 @echo off
 @cls
 start D:\APPS\MIRAI\Run.bat
+git pull
 ping 127.0.0.1 > nul
 ping 127.0.0.1 > nul
 ping 127.0.0.1 > nul
 :Core
 Node --inspect=9222 OnlineAi.js
+git push
 if exist _NotReStart exit
 goto Core
