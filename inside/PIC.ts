e3c086fecae654521e91fa5e96a6524b35abaaff
Lanshuye <927980270@qq.com>
2022-01-14 13:10:15 +0800

    inside OS Update0

--- inside/PIC.ts
+++ inside/PIC.ts
@@ -1,5 +1,5 @@
 // Picer
-import * as Core from "./../Core"
+import * as Core from "./Core"
 Core.AddListener((c,i)=>{
     if(i.message.substr(0,3) == "PIC"){
         var u = i.message.replace("PIC","");
